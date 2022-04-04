08ab2d097c12d02075b590a4ba1e6e2008976a29
Arun MR <arun_mr549e@protonmail.com>
2022-04-04 16:45:04 +0530

    Email sender updated

--- email_sender/QueueUtil.py
+++ email_sender/QueueUtil.py
@@ -8,24 +8,6 @@ sqs = boto3.client('sqs')
 queue_url = os.getenv('EMAIL_SQS_QUEUE_URL')
 max_number_of_messages = os.getenv('EMAIL_MAX_NUMBER_OF_MESSAGES')
 
-def get_messages():
-    messages = []
-    response = sqs.receive_message(
-        QueueUrl=queue_url,
-        AttributeNames=[
-            'SentTimestamp'
-        ],
-        MaxNumberOfMessages=int(max_number_of_messages),
-        MessageAttributeNames=[
-            'All'
-        ],
-        # VisibilityTimeout=0,
-        WaitTimeSeconds=0
-    )
-    if 'Messages' in response:
-        messages = response['Messages']
-    return messages
-
 
 def delete_message(message):
     receipt_handle = message['ReceiptHandle']
--- email_sender/app.py
+++ email_sender/app.py
@@ -3,46 +3,38 @@ import json
 import EmailSender as email_sender
 import QueueUtil as queue_util
 import ESLogger as eslogger
-import ErrorProcessor as error_processor
 from botocore.exceptions import ClientError
 
 
 """ Event Bridge Trigger """
 def lambda_handler_sqs(event, context):
-    messages = queue_util.get_messages()
-    if len(messages) == 0:
-        eslogger.info("No messages found in queue")
-    else:
-        eslogger.info("Received messages count : " + str(len(messages)))
 
-    failed_messages = []
-    for message in messages:
-        eslogger.info(message)
-        body = message['Body']
+    eslogger.info("event aka message :-")
+    eslogger.info(event)
+
+    message = event
+
+    try:
+        body = message['body']
         payload = json.loads(body)
-        try:
-            response = email_sender.send_email(payload)
-            queue_util.delete_message(message)
-        except ClientError as e:
-            eslogger.info("Printing error in handler")
-            eslogger.error(e)
-            eslogger.info("Printing error response in handler")
-            eslogger.error(e.response)
-            to_be_suppressed_list = error_processor.get_to_be_suppressed_list(e.response)
-            email_sender.add_to_suppression_list(to_be_suppressed_list)
-            failed_messages.append(message)
-
-    if len(failed_messages) > 0:
-        eslogger.info("Failed message count : " + str(len(failed_messages)))
+        eslogger.info("payload :-")
+        eslogger.info(payload)
+        response = email_sender.send_email(payload)
+        queue_util.delete_message(message)
+    except ClientError as e:
+        eslogger.info("Printing error in handler")
+        eslogger.error(e)
+        eslogger.info("Printing error response in handler")
+        eslogger.error(e.response)
 
     return get_response_from_input(event)
 
 
 def get_response_from_input(event):
     response = {}
-    if event is not None and 'iteration_count' in event:
-        response = {
-            'iteration_count': event['iteration_count'],
-            'iteration_max_count': event['iteration_max_count']
-        }
+    # if event is not None and 'iteration_count' in event:
+    #     response = {
+    #         'iteration_count': event['iteration_count'],
+    #         'iteration_max_count': event['iteration_max_count']
+    #     }
     return response
