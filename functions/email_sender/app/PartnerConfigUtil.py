7cbb3cbfcec31dea793be77175bc289b40bacd10
Arun MR <arun.mr@hiddeninsight.in>
2022-08-25 16:55:54 +0530

    PartnerUtil updated to load attachment configuration

--- functions/email_sender/app/PartnerConfigUtil.py
+++ functions/email_sender/app/PartnerConfigUtil.py
@@ -12,4 +12,24 @@ def get_partner_configuration(partner_key):
     config_json = file_reader.get_file_as_text(bucket_name, folder_name, file_name)
     return json.loads(config_json)
 
+def get_attachment_files_config(partner_key, client_key):
+    partner_config = get_partner_configuration(partner_key)
+    
+    bucket_name = None
+    folder_name = None
+    if client_key is None:
+        bucket_name = partner_config['s3_bucket_name']
+        folder_name = partner_config['s3_folder_name']
+    else:
+        for client in partner_config['clients']:
+            if client['client_key'] == client_key:
+                folder_name = client['s3_folder_name']
+                if 's3_bucket_name' in client:
+                    bucket_name = client['s3_bucket_name']
+                else:
+                    bucket_name = partner_config['s3_bucket_name']
 
+    return {
+             "bucket_name": bucket_name,
+             "folder_name": folder_name
+            }
--- tests/email_sender/test_partner_config_util.py
+++ tests/email_sender/test_partner_config_util.py
@@ -16,3 +16,21 @@ class PartnerConfigUtilTest(unittest.TestCase):
         self.assertIsNotNone(partner_config)
         self.assertEqual(partner_key, partner_config["partner_key"])
 
+    def test_get_attachment_files_config(self):
+        partner_key = "hiddeninsight-key-9643"
+
+        """Case 1 - Only partner configuration. No clients"""
+        files_config = config_util.get_attachment_files_config(partner_key, None)
+        self.assertEqual("email-app-attachmentsbucket-5c4l0y3hobmw", files_config['bucket_name'])
+        self.assertEqual("hiddeninsight", files_config['folder_name'])
+
+        """Case 2 - Client share same bucket with email app"""
+        files_config = config_util.get_attachment_files_config(partner_key, "mountlitera-key-1238")
+        self.assertEqual("email-app-attachmentsbucket-5c4l0y3hobmw", files_config['bucket_name'])
+        self.assertEqual("mountlitera", files_config['folder_name'])
+
+        """Case 3 - Client has own bucket and folder"""
+        files_config = config_util.get_attachment_files_config(partner_key, "greenchalkps-key-8528")
+        self.assertEqual("ygiuhiuhiujijioji", files_config['bucket_name'])
+        self.assertEqual("gcps", files_config['folder_name'])
+
