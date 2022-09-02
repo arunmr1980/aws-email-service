23f3252784d63202a0b4101cbd2b976393e9586b
Arun MR <arun.mr@hiddeninsight.in>
2022-09-02 11:25:40 +0530

    config value updates for a fresh stack setup

--- setup/env_setup.sh
+++ setup/env_setup.sh
@@ -1,4 +1,4 @@
-export ATTACHMENT_S3_BUCKET=email-app-attachmentsbucket-lolf8zbjv9a0
-export EMAIL_PROCESSOR_STATE_MACHINE_ARN=arn:aws:states:us-west-2:272936841180:stateMachine:EmailProcessingStateMachine-zAMPhE7O5niB
+export ATTACHMENT_S3_BUCKET=email-app-attachmentsbucket-1ejmf5a42pyiq
+export EMAIL_PROCESSOR_STATE_MACHINE_ARN=arn:aws:states:us-west-2:272936841180:stateMachine:EmailProcessingStateMachine-osSpHHyttQns
 export BUCKET_SUFFIX=6542
 export SNS_TOPIC_ARN=arn:aws:sns:us-west-2:272936841180:EmailSNSTopic.fifo
