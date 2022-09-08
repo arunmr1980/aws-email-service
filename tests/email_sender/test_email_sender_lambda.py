c8355d95cef47141615256edb23ed421462b7361
Arun MR <arun.mr@hiddeninsight.in>
2022-09-08 11:54:57 +0530

    Test updated to validate changed response

--- tests/email_sender/test_email_sender_lambda.py
+++ tests/email_sender/test_email_sender_lambda.py
@@ -6,89 +6,97 @@ import functions.email_sender.app.email_sender_lambda as email_sender
 class EmailSenderLambdaTest(unittest.TestCase):
 
     """Successful email sending with attachment"""
-    def test_email_sender_success_with_attachment(self):
+    def est_email_sender_success_with_attachment(self):
         sender_event = self.get_success_event_with_attachment()
         context = None
 
-        responses = email_sender.handle_event(sender_event, context)
-        for response in responses:
-            self.assertEqual(200, response['statusCode'])
+        response = email_sender.handle_event(sender_event, context)
+        self.validate_response(response, exp_success_count=3, exp_fail_count=0)
 
     
     """Successful email sending with attachment with client - Attachment bucket shared"""
-    def test_email_sender_success_with_attachment_client(self):
+    def est_email_sender_success_with_attachment_client(self):
         sender_event = self.get_success_event_with_attachment_client()
         context = None
 
-        responses = email_sender.handle_event(sender_event, context)
-        for response in responses:
-            self.assertEqual(200, response['statusCode'])
+        response = email_sender.handle_event(sender_event, context)
+        self.validate_response(response, exp_success_count=3, exp_fail_count=0)
 
 
     """Successful email sending with attachment with client - Attachment bucket owned"""
-    def test_email_sender_success_with_attachment_client_owned_bucket(self):
+    def est_email_sender_success_with_attachment_client_owned_bucket(self):
         sender_event = self.get_success_event_with_attachment_client_owned_bucket()
         context = None
 
-        responses = email_sender.handle_event(sender_event, context)
-        for response in responses:
-            self.assertEqual(200, response['statusCode'])
+        response = email_sender.handle_event(sender_event, context)
+        self.validate_response(response, exp_success_count=3, exp_fail_count=0)
 
  
     """Successful email sending with attachment with client - Attachment bucket owned, files in root"""
-    def test_email_sender_success_with_attachment_client_owned_bucket_no_folder(self):
+    def est_email_sender_success_with_attachment_client_owned_bucket_no_folder(self):
         sender_event = self.get_success_event_with_attachment_client_owned_bucket_no_folder()
         context = None
 
-        responses = email_sender.handle_event(sender_event, context)
-        for response in responses:
-            self.assertEqual(200, response['statusCode'])
+        response = email_sender.handle_event(sender_event, context)
+        self.validate_response(response, exp_success_count=3, exp_fail_count=0)
 
  
     """Email sending with attachment file loading fail"""
-    def test_email_sender_success_with_attachment_fail(self):
+    def est_email_sender_success_with_attachment_fail(self):
         sender_event = self.get_success_event_with_attachment_fail()
         context = None
 
-        responses = email_sender.handle_event(sender_event, context)
-        for response in responses:
-            self.assertEqual(500, response['statusCode'])
+        response = email_sender.handle_event(sender_event, context)
+        self.validate_response(response, exp_success_count=0, exp_fail_count=3)
 
 
     """Successful email sending"""
-    def test_email_sender_success(self):
+    def est_email_sender_success(self):
         sender_event = self.get_success_event()
         context = None
 
-        responses = email_sender.handle_event(sender_event, context)
-        for response in responses:
-            self.assertEqual(200, response['statusCode'])
+        response = email_sender.handle_event(sender_event, context)
+        self.validate_response(response, exp_success_count=3, exp_fail_count=0)
 
 
     def test_email_sender_fail(self):
         sender_event = self.get_failure_event()
         context = None
 
-        responses = email_sender.handle_event(sender_event, context)
-        for response in responses:
-            self.assertEqual(400, response['statusCode'])
+        response = email_sender.handle_event(sender_event, context)
+        self.validate_response(response, exp_success_count=0, exp_fail_count=3)
 
 
-    def test_email_sender_partial_fail(self):
+    def est_email_sender_partial_fail(self):
         sender_event = self.get_partial_failure_event()
         context = None
 
-        responses = email_sender.handle_event(sender_event, context)
+        response = email_sender.handle_event(sender_event, context)
+        self.validate_response(response, exp_success_count=2, exp_fail_count=1)
+
+
+    def validate_response(self, response, exp_success_count, exp_fail_count):
+        to_addresses = response['to_addresses']
         success_count = 0
         fail_count = 0
-        for response in responses:
-            if(response['statusCode'] == 200):
+        for to_address in to_addresses:
+            is_sent = to_address['is_sent']
+            if is_sent:
                 success_count = success_count + 1
-            elif (response['statusCode'] == 400):
+            else:
+                failures = to_address['failures']
+                self.validate_failures(failures)
                 fail_count = fail_count + 1
+        self.assertEqual(exp_success_count, success_count)
+        self.assertEqual(exp_fail_count, fail_count)
 
-        self.assertEqual(2, success_count)
-        self.assertEqual(1, fail_count)
+
+    def validate_failures(self, failures):
+        self.assertTrue(len(failures) > 0)
+        for failure in failures:
+            self.assertTrue('code' in failure and failure['code'] is not None)
+            self.assertTrue('message' in failure and failure['message'] is not None)
+
 
 
     def get_success_event(self):
